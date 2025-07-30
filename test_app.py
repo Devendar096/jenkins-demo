a2813b88dfb807f922d8eede524f3fe56cc48d8a
LakshmiChandana10 <lakshmichandana010@gmail.com>
2025-07-30 11:22:27 +0530

    changes in test

--- test_app.py
+++ test_app.py
@@ -8,7 +8,7 @@ class FlaskAppTestCase(unittest.TestCase):
     def test_home(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Hello from Docker and Flask', response.data)
+        self.assertIn(b'Hello from Flask & Docker - V2', response.data)
 
 if __name__ == '__main__':
     unittest.main()
